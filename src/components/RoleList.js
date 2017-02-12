09fcdbdec3518ce639e99681504023ecb6304007
lark <larkl4rk@gmail.com>
2017-02-12 23:53:20 +0900

    Add RoleList's active class

--- src/components/RoleList.js
+++ src/components/RoleList.js
@@ -6,26 +6,39 @@ class RoleList extends Component {
 
         this.state = {
             items: [
-                {key: null, text: '전체'},
-                {key: 'Assassin', text: '암살자'},
-                {key: 'Fighter', text: '전사'},
-                {key: 'Mage', text: '마법사'},
-                {key: 'Support', text: '서포터'},
-                {key: 'Tank', text: '탱커'},
-                {key: 'Marksman', text: '원거리 딜러'}
-            ]
+                {role: null, text: '전체'},
+                {role: 'Assassin', text: '암살자'},
+                {role: 'Fighter', text: '전사'},
+                {role: 'Mage', text: '마법사'},
+                {role: 'Support', text: '서포터'},
+                {role: 'Tank', text: '탱커'},
+                {role: 'Marksman', text: '원거리 딜러'}
+            ],
+            activeRole: null,
+            activeClass: 'role__item--active'
         };
     }
 
     updateRole(role) {
-        this.props.onUpdateRole(role);
+        this.setState({activeRole: role});
+        this.props.onUpdateRole(this.state.activeRole);
+    }
+
+    activeClass(role) {
+        let className = '';
+        if (
+            (this.state.activeRole === role) || (this.state.activeRole === '')
+        ) {
+            className = this.state.activeClass;
+        }
+        return className;
     }
 
     render() {
         let items = this.state.items.map((item, idx) => (
-            <li className="role__item" key={idx}>
+            <li className={'role__item ' + this.activeClass(item.role)} key={idx}>
                 <button className="role__item-button"
-                        onClick={this.updateRole.bind(this, item.key)}>{item.text}</button>
+                        onClick={this.updateRole.bind(this, item.role)}>{item.text}</button>
             </li>
         ));
 
